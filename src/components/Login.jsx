036922e3d92517113132c5658e898482995f78c2
Vinayak Chittora <vinayakchittora@gmail.com>
2024-04-17 14:22:02 +0530

    login-signup functionality created

---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email,
+      password,
+    };
+
+    fetch("http://localhost:9000/login", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        // console.log(response.ok);
+        if (response.ok) {
+          console.log("user logged in succesfully");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column", width: "250px" }}
+    >
+      <h1>Login</h1>
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter your password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email,
+      password,
+    };
+
+    // console.log(formData);
+
+    fetch("http://localhost:9000/signup", {
+      mode: "cors",
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("User has been successfully signed up");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("There was an error sending the form data to the server");
+      });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column", width: "250px" }}
+    >
+      <h1>Sign up</h1>
+      <input
+        type="email"
+        name="emailofuser"
+        placeholder="Enter your email"
+        // value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="passwordofuser"
+        placeholder="Enter your password"
+        // value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Signup;
