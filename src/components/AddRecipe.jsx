bb3808d9e2643b2b5665c3cf2f2819f362e0c833
Vinayak Chittora <vinayakchittora@gmail.com>
2024-04-19 10:34:04 +0530

    addRecipe modified

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const AddRecipe = () => {
-  return (
-    <div>
-      <p>this is add-recipe</p>
-    </div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [img, setImg] = useState("");
+  const [desc, setDesc] = useState("");
 
-export default AddRecipe
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      title,
+      desc,
+      img,
+    };
+
+    fetch("http://localhost:9000/recipe/add-recipe", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        // console.log(response.ok);
+        if (response.ok) {
+          console.log("recipe added successfully");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column", width: "250px" }}
+    >
+      <h1>Add Recipe</h1>
+      <input
+        type="text"
+        name="title"
+        placeholder="Enter title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        name="desc"
+        placeholder="Enter desc"
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <input
+        type="text"
+        name="img"
+        placeholder="Enter image url"
+        onChange={(e) => setImg(e.target.value)}
+      />
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
+
+export default AddRecipe;
